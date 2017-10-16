d70fcf6889059ed59f6c7f20138c3e0e88b96648
Marcin <141marcin@gmail.com>
2017-10-17 00:33:23 +0200

    bad name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FirstComponent from './Component/FirstComponent.js';
+import Menu from './Component/Menu.js';
 import Home from './Component/Home.js';
 
 class App extends Component {
@@ -11,7 +11,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-           	<FirstComponent/>
+           	<Menu/>
         </header>
         <Home/>
       </div>
